77c04d1255d272e53d46c7f72e033cba24926796
Growstale <113666093+Growstale@users.noreply.github.com>
2022-12-19 00:56:16 +0300

    DrawField.h
    Заголовочный файл, отвечающий за прорисовку

---
+++ DrawField.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+
+extern char field[19][19];//массив символов дл€ отрисовки
+extern int applex, appley;//начальные координаты €блок
+void drawApple();
