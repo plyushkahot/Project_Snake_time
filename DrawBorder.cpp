91d84e2d5ad295d68de89749bbb1ea27766a9735
antikvar <112938626+Antikvar23e4@users.noreply.github.com>
2022-12-20 01:03:42 +0300

    Update DrawBorder.cpp

--- DrawBorder.cpp
+++ DrawBorder.cpp
@@ -1,14 +1,16 @@
+
+#include "DrawField.h";
 void drawBorder()
 {
 	char border = char(178);
-	for (int i = 1; i < n ; i++)
+	for (int i = 1; i < n - 1; i++)
 	{
 		field[i][0] = border;
-		field[i][m] = border;
+		field[i][m - 1] = border;
 	}
 	for (int i = 0; i < m; i++)
 	{
 		field[0][i] = border;
-		field[n][i] = border;
+		field[n - 1][i] = border;
 	}
 }
