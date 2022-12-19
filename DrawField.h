9dd70b26006e0917e5ca508dc7b52957ea071b8e
antikvar <112938626+Antikvar23e4@users.noreply.github.com>
2022-12-20 01:05:34 +0300

    Update DrawField.h

--- DrawField.h
+++ DrawField.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <Windows.h>
 
+extern int n, m;//размер сетки
 extern char field[19][19]; //массив символов для отрисовки
 extern int applex, appley; //начальные координаты яблок
 void drawApple();
+void drawBorder();
+void cleanField();
